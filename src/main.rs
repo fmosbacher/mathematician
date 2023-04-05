2bcb5cdef2e7b8d6471a30058e88446d47278640
Fernando Azevedo <azevedo.fma@gmail.com>
2023-04-05 12:23:46 -0300

    remove failed attempt of parsing inside the lexer

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-use mathematician::Lexer;
+use mathematician::lexer::Lexer;
 
 fn main() {
     let mut lexer = Lexer::default();
-    assert_eq!(lexer.tokenize("12 + 3"), Ok(()));
+    assert_eq!(lexer.tokenize("2+2+-365*/"), Ok(()));
     lexer.tokens().for_each(|token| println!("{:?}", token));
-    // Will print:
-    // Integer(12)
-    // Plus
-    // Integer(3)
 }
